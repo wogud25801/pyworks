c8a6a326de1428e943e3b01f78e26f2546fd56f9
woghd25801 <zx1148@naver.com>
2022-01-25 11:05:59 +0900

    pyworks

---
+++ day17/data.py
@@ -0,0 +1,22 @@
+#데이터 출력
+#숫자
+print(5)
+print(2.54)
+print(3 + 4)
+print(3 + int("4"))
+
+#문자
+print("안녕")
+print("3" + "4")
+#print(3 + "4")
+print(str(3) + "4")
+
+#불리언
+print(10 > 11)
+print(10 == 10)
+
+#배열(리스트)
+seasons = ["봄", "여름", "가을", "겨울"]
+print(seasons)
+print(seasons[0])
+print(seasons[-1])
