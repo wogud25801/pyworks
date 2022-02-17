826b51f654351d44e344e99b73173d2eedd61e26
woghd25801 <zx1148@naver.com>
2022-02-17 12:48:04 +0900

    pyworks

--- day31/website/app.py
+++ day31/website/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/season/')
+@app.route('/season/', methods = ['GET'])
 def season():
     season = "겨울"
     seasons = ["봄", "여름", "가을", "겨울"]
@@ -20,5 +20,53 @@ def season():
 def loop_index():
     items = ['a', 'b', 'c', 'd']
     return render_template('loop_index.html', items=items)
+
+@app.route('/register/', methods = ['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        # 데이터 전달 받음(가져오기)
+        mid = request.form['mid']
+        pwd = request.form['passwd']
+        name = request.form['name']
+        gender = request.form['gender']
+
+        # 데이터를 memberlist에 보냄
+        return render_template(
+            'memberlist.html',
+            mid=mid,
+            pwd=pwd,
+            name=name,
+            gender=gender
+    )
+
+    else:  # request.method == 'GET'
+        return render_template('register.html')
+
+@app.route('/even_odd/', methods=['GET', 'POST'])
+def even_odd():
+    if request.method == "POST":
+        try:
+            # 데이터 수집
+            num = int(request.form['num'])  #문자를 숫자로 변형함
+        except ValueError:
+            error = "숫자를 입력하세요."
+            return render_template('even_odd.html', error_msg=error)
+
+        # 데이터 연산
+        if num % 2 == 0:
+            result = "짝수 입니다."
+        else:
+            result = "홀수 입니다."
+
+        #데이터 보내기
+        return render_template(
+            'calc_result.html',
+            num=num,
+            result=result
+        )
+    else:
+        return render_template('even_odd.html')
+
+
 
 app.run()
---
+++ day31/website/static/main_js.js
@@ -0,0 +1,17 @@
+// 마우스 이벤트 -
+// 마우스 올리기 - onmouseover, 마우스 내리기 - onmouseout
+//addEventListener() - mouseover, mouseout 사용(on 생략)
+let pic = document.getElementById("pic");
+//pic.onmouseover = changePic;  // 함수 호출시 괄호 생략
+//pic.onmouseout = originPic;
+
+pic.addEventListener('mouseover', changePic);
+pic.addEventListener('mouseout', originPic);
+
+function changePic(){
+    pic.src = '../static/coffee-blue.jpg';
+}
+
+function originPic(){
+    pic.src = '../static/coffee-gray.jpg';
+}
